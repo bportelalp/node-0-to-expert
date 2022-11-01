bd54fd97a07f00ad153bd661a8bd74e03ed9336c
bportela <bportela.dev@gmail.com>
2022-11-01 22:00:57 +0100

    46. stdin stdout Readline

---
+++ 04-todo-list-console/helpers/mensajes.js
@@ -0,0 +1,34 @@
+const colors = require('colors');
+
+const mostrarMenu = () => {
+    console.clear();
+    console.log('=========================='.green);
+    console.log('  Seleccione una opción'.green);
+    console.log('==========================\n'.green);
+
+    console.log(`${'1.'.green} Crear tarea`);
+    console.log(`${'2.'.green} Listar tareas`);
+    console.log(`${'3.'.green} Listar tareas completadas`);
+    console.log(`${'4.'.green} Listar tareas pendientes`);
+    console.log(`${'5.'.green} Completar tarea(s)`);
+    console.log(`${'6.'.green} Borrar tarea`);
+    console.log(`${'0.'.green} Salir\n`);
+}
+
+const pausa = () => {
+    const readline = require('readline').createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    readline.question(`Presione ${'ENTER'.green} para continuar\n`, (opt) => {
+
+        readline.close();
+    })
+}
+
+
+module.exports = {
+    mostrarMenu,
+    pausa
+}
