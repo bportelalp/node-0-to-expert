34780ef8b673da72df8bb6ac24b25364bbd1464e
bportela <bportela.dev@gmail.com>
2022-11-01 17:19:11 +0100

    32. Recibir argumentos de consola

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,8 +1,12 @@
 const { crearArchivo } = require('./helpers/multiplicar')
+
+
 console.clear();
 
-const base = 7;
-
+// extraer de la consola de comandos, el valor de Base.
+// 
+const [,,arg3] = process.argv
+const [,base = 5] = arg3.split('=')
 
 crearArchivo(base)
     .then(r => console.log(r))
