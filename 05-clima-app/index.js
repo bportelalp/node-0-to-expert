9b668b1920d01836b2d52768eeb328d2c91648e3
bportela <bportela.dev@gmail.com>
2022-11-09 19:29:02 +0100

    68. Menú app

--- 05-clima-app/index.js
+++ 05-clima-app/index.js
@@ -4,7 +4,20 @@ import * as ui from "./helpers/inquirer.js";
 console.log('Iniciando app\n');
 
 const main = async () => {
-    const texto = await ui.leerInput("Cosas");
+    let opt = 0;
+    do {
+        opt = await ui.inquirerMenu();
+        switch (opt) {
+            case 1:
+                break;
+            case 2:
+                break;
+            default:
+                break;
+        }
+
+        await ui.pausa();
+    } while (opt !== 0);
 }
 
 await main();
